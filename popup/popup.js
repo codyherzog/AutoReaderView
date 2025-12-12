b80d2d11c3b5d94a5fc2639ef476ddb387f61653
Cody Herzog <codyherzog@gmail.com>
2025-12-12 00:36:08 -0800

    Incremental UI work

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const myButton = document.getElementById("toggle");
+    const myButton = document.getElementById("apply");
     myButton.addEventListener("click", () => {
+        const whitelist = document.getElementById("whitelist");
+        const blacklist = document.getElementById("blacklist");
         console.log("Button clicked in extension page!");
     });
 });
